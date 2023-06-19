100248b60d0f53b5ddb4b16b2428e4b3d8664729
PrzemyslawRewis <przemyslaw_rewis@icloud.com>
2023-06-19 18:26:28 +0200

    added setup, basic page and button to calculate path

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # app
+To run enter command: python3 -m http.server in terminal
+and run http://0.0.0.0:8000 in your browser
---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+import base64
+import cv2
+import json
+import js
+import numpy as np
+import pyscript
+from pyodide.http import pyfetch
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+import numpy as np
+
+
+async def calculateShortestPath():
+    #TODO: Logic to calculate the shortest path
+    print('Calculating shortest path')
+    print('Your open-cv version is:')
+    print(cv2.__version__)
+    
+def drawResult():
+   x_values = [1, 2, 3, 4, 5]
+   y_values = [2, 4, 6, 8, 10]
+   x = plt.plot(x_values, y_values)
+   plt.show(x)
+
+
