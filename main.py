c6ad0144d7a8cb0f07b64a13df9d0b6241cc263a
PrzemyslawRewis <przemyslaw_rewis@icloud.com>
2023-07-05 10:59:07 +0200

    added v2 frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import cv2
 import json
 import js
@@ -12,6 +13,7 @@ import math
 from matplotlib import pyplot
 import random
 import pyodide
+
 
 
 # pyodide.register_js_function(load_image)
@@ -37,8 +39,7 @@ imageName="test.jpg"
 #rozmiar obrazu powyżej (nie dawać więcej niż 512 bo nie testowane)
 textureSize=512
 
-def calculateShortestPath():
-
+def calculate_shortest_path():
     ants=[Ant(start[0],start[1]) for i in range(antNumber)]
     tmp=round(textureSize*math.sqrt(2))
     pheromones = [[tmp-round(math.sqrt(math.pow(i-goal[0],2)+math.pow(j-goal[1],2))) for j in range(textureSize)] for i in range(textureSize)]
@@ -47,45 +48,31 @@ def calculateShortestPath():
     while (Ant.desiredPathNumber>0):
         for i in range(len(ants)):
             ants[i].move(pheromones, img)
-    #TODO: Logic to calculate the shortest path
 
     for j in range(512):
         d=[i for i in Ant.paths[0][j] if i]
         print(d)
-    drawResult()
-    readFile("test.jpg")
-    showImage()
-             
-    
-def drawResult():
+    show_result_image()
+
+
+
+def show_input_image():
     fig, ax = plt.subplots()
-    # x axis
-    x = ["Python", "C++", "JavaScript", "Golang"]
-    # y axis
-    y = [10, 5, 9, 7]
-    plt.plot(x, y, marker='o', linestyle='-', color='b')
-    # Naming the x-label
-    plt.xlabel('Language')
-    # Naming the y-label
-    plt.ylabel('Score')
-    
-    # Naming the title of the plot
-    plt.title('Language vs Score')
-    display(fig,target="matplotlib-lineplot")
-    
+    image = cv2.imread(imageName)
+    ax.imshow(image)
+    ax.invert_yaxis()
+    display(fig, target="input")
 
-def fileUpload():
-    print("upload file")
+def show_points():
+    display(start, target="points")
+    display(goal, target="points")
 
-def readFile(filename):
-    display("wczytana tablica",target="matplotlib-lineplot")
-    display(cv2.imread(filename),target="matplotlib-lineplot")
 
-def showImage():
+
+def show_result_image():
     fig, ax = plt.subplots()
     plt.imshow(cv2.imread("test.jpg"))
-    display("wczytany obraz",target="matplotlib-lineplot")
-    display(fig, target="matplotlib-lineplot")
+    display(fig, target="result")
 
 def bgr2rgb(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
